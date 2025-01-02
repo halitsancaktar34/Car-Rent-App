261627063f8e2db7c03d17cf173c76219262b8fc
Halit Sancaktar <141269714+halitsancaktar34@users.noreply.github.com>
2025-01-02 16:43:40 +0300

    Update fetchCars.ts

--- src/utils/fetchCars.ts
+++ src/utils/fetchCars.ts
@@ -3,8 +3,6 @@ import { CarType, FilterType } from "../types";
 const options = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "53bbfd6a45mshba00130ed68b464p12d86djsn5d5b28fac991",
-    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   },
 };
 export async function fetchCars(filters: FilterType): Promise<CarType[]> {
@@ -16,7 +14,6 @@ export async function fetchCars(filters: FilterType): Promise<CarType[]> {
     fuel_type = '',
   } = filters;
   const res = await fetch(
-    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${make}&model=${model}&limit=${limit}&year=${year}&fuel_type=${fuel_type}`,
     options
   );
   return await res.json();
